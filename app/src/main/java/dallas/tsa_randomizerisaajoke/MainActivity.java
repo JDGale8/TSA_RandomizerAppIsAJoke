d627c5e15f6bb9786501ae87f7574892645c4cf7
Jake <Jake@JakeGale.com>
2016-04-07 00:27:12 +0100

    Initial Commit
    Project is made in Android studio - Arrows have been added to drawables,
    two placeholding images and a big button form the main screen.
    No Java coding has begun.

---
+++ app/src/main/java/dallas/tsa_randomizerisaajoke/MainActivity.java
@@ -0,0 +1,16 @@
+package dallas.tsa_randomizerisaajoke;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    
+
+}
