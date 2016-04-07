c4cf31f99cd106a4b65c39e38a071a6306b4ac25
Jake <Jake@JakeGale.com>
2016-04-07 07:55:09 +0100

    Base_Working_Functionality
    Added functionality to the main button. It now produces a left or right
    arrow when clicked.

--- app/src/main/java/dallas/tsa_randomizerisaajoke/MainActivity.java
+++ app/src/main/java/dallas/tsa_randomizerisaajoke/MainActivity.java
@@ -2,15 +2,52 @@ package dallas.tsa_randomizerisaajoke;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ImageView leftArrow = (ImageView) findViewById(R.id.leftArrow);
+        final ImageView rightArrow = (ImageView) findViewById(R.id.rightArrow);
+        final Button btn = (Button) findViewById(R.id.randomizeButton);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                leftArrow.setVisibility(View.GONE);
+                rightArrow.setVisibility(View.GONE);
+                btn.setVisibility(View.INVISIBLE);
+
+                int arrowNumber = getArrow();
+                Log.d(TAG, arrowNumber+"");
+                if (arrowNumber == 1) leftArrow.setVisibility(View.VISIBLE);
+                else rightArrow.setVisibility(View.VISIBLE);
+            }
+        });
+
+
+
+
     }
 
-    
+    public static int getArrow(){
+        Random random = new Random();
+        int arrowNumber = random.nextInt(2);
+        return arrowNumber;
+    };
+
 
 }
