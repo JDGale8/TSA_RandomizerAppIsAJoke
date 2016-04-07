467072d965269e8f25fa81c57bd44e0e84c1f57e
Jake <Jake@JakeGale.com>
2016-04-07 08:21:19 +0100

    add_animations
    Added short animations to the arrows so they appear and fade away before
    you can press again.

--- app/src/main/java/dallas/tsa_randomizerisaajoke/MainActivity.java
+++ app/src/main/java/dallas/tsa_randomizerisaajoke/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
 import android.widget.Button;
 import android.widget.ImageView;
 
@@ -24,17 +26,46 @@ public class MainActivity extends AppCompatActivity {
         final ImageView rightArrow = (ImageView) findViewById(R.id.rightArrow);
         final Button btn = (Button) findViewById(R.id.randomizeButton);
 
+        final Animation arrowFadeOut = new AlphaAnimation(1,0);  // parameters are 'from alpha', 'to alpha'
+        arrowFadeOut.setStartOffset(500); // When the animation should start relative to start time
+        arrowFadeOut.setDuration(500);
+        leftArrow.setAnimation(arrowFadeOut);
+        rightArrow.setAnimation(arrowFadeOut);
+
+        arrowFadeOut.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                leftArrow.setVisibility(View.GONE);
+                rightArrow.setVisibility(View.GONE);
+                btn.setVisibility(View.VISIBLE);
+            }
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 leftArrow.setVisibility(View.GONE);
                 rightArrow.setVisibility(View.GONE);
-                btn.setVisibility(View.INVISIBLE);
+                btn.setVisibility(View.GONE);
 
                 int arrowNumber = getArrow();
-                Log.d(TAG, arrowNumber+"");
-                if (arrowNumber == 1) leftArrow.setVisibility(View.VISIBLE);
-                else rightArrow.setVisibility(View.VISIBLE);
+                Log.d(TAG, "Arrow number is " + arrowNumber);
+                if (arrowNumber == 1) {
+                    leftArrow.setVisibility(View.VISIBLE);
+                    leftArrow.startAnimation(arrowFadeOut);
+                }
+                else {
+                    rightArrow.setVisibility(View.VISIBLE);
+                    rightArrow.startAnimation(arrowFadeOut);
+                }
             }
         });
 
