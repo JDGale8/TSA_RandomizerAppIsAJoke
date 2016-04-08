1c1d22b52d8e07892861fb3ad47a61d27e5f168a
Jake <Jake@JakeGale.com>
2016-04-08 23:49:53 +0100

    fixed typo - from TSA_RandomizerIsaAJoke, to TSA_RandomizerisAJoke
    t

---
+++ app/src/main/java/dallas/tsa_randomizerisajoke/MainActivity.java
@@ -0,0 +1,86 @@
+package dallas.tsa_randomizerisaajoke;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.d(TAG, "MAIN ACTIVITY LOADED");
+
+        final ImageView leftArrow = (ImageView) findViewById(R.id.leftArrow);
+        final ImageView rightArrow = (ImageView) findViewById(R.id.rightArrow);
+        final Button btn = (Button) findViewById(R.id.randomizeButton);
+
+        final Animation arrowFadeOut = new AlphaAnimation(1,0);  // parameters are 'from alpha', 'to alpha'
+        arrowFadeOut.setStartOffset(500); // When the animation should start relative to start time
+        arrowFadeOut.setDuration(500);
+        leftArrow.setAnimation(arrowFadeOut);
+        rightArrow.setAnimation(arrowFadeOut);
+
+        arrowFadeOut.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                leftArrow.setVisibility(View.GONE);
+                rightArrow.setVisibility(View.GONE);
+                btn.setVisibility(View.VISIBLE);
+            }
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                leftArrow.setVisibility(View.GONE);
+                rightArrow.setVisibility(View.GONE);
+                btn.setVisibility(View.GONE);
+
+                int arrowNumber = getArrow();
+                Log.d(TAG, "Arrow number is " + arrowNumber);
+                if (arrowNumber == 1) {
+                    leftArrow.setVisibility(View.VISIBLE);
+                    leftArrow.startAnimation(arrowFadeOut);
+                }
+                else {
+                    rightArrow.setVisibility(View.VISIBLE);
+                    rightArrow.startAnimation(arrowFadeOut);
+                }
+            }
+        });
+
+
+
+
+    }
+
+    public static int getArrow(){
+        Random random = new Random();
+        int arrowNumber = random.nextInt(2);
+        return arrowNumber;
+    };
+
+
+}
---
+++ app/src/main/java/dallas/tsa_randomizerisajoke/WelcomePage.java
@@ -0,0 +1,36 @@
+package dallas.tsa_randomizerisaajoke;
+
+import android.content.Intent;
+import android.os.StrictMode;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+/* Welcome page for the TSA Randomizing arrow app */
+
+
+public class WelcomePage extends AppCompatActivity {
+
+    private static final String TAG = WelcomePage.class.getSimpleName();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.welcome_page);
+        Log.d(TAG, "Welcome page is loaded");
+
+        final Button advanceBtn = (Button) findViewById(R.id.intializeActivity);
+
+        advanceBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mainIntent = new Intent("dallas.tsa_randomizerisaajoke.MainActivity");
+                startActivity(mainIntent);
+            }
+        });
+
+    }
+}
